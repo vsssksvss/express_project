8f3fa08b6b093457dce8fc30b5fc911d21c9a055
vsssksvss <madnoja28@gmail.com>
2025-03-18 18:57:33 +0900

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+  });
+  app.get('/best', (req, res) => {
+    res.send('sexyGirl');
+  });
